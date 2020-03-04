2bd154500d9f4f68c461bf17598f31eb20501b3d
Benjamin Tikki Olsen <benjamin.tikki@gmail.com>
2020-03-04 19:10:11 +0100

    Test

---
+++ VigenereCipher/VigenereCipher/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VigenereCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ".ToCharArray();
+
+
+            int i, j, counter;
+            char[,] arr1 = new char[29, 29];
+
+            Console.Write("\n\nRead a 2D array of size 3x3 and print the matrix :\n");
+            Console.Write("------------------------------------------------------\n");
+
+
+            /* Stored values into the array*/
+            Console.Write("Input elements in the matrix :\n");
+            for (i = 0; i < 29; i++)
+            {
+                for (j = 0; j < 29; j++)
+                {
+                    arr1[i, j] = alphabet[j];
+                }
+            }
+
+            for (i = 0; i < 29; i++)
+            {
+                for (j = 0; j < 29; j++)
+                {
+
+                    counter = j + i;
+                    if (counter >= 29)
+                    {
+                        counter = 0;
+                        counter++;
+                    }
+
+                    Console.Write(arr1[i, counter] + " ");
+                    counter++;
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.Read();
+        }
+
+
+
+    }
+}
